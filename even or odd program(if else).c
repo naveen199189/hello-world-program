4c1b7e77d1337de48a8ea15f11cad6b5919639e6
naveen199189 <106946229+naveen199189@users.noreply.github.com>
2022-06-07 16:04:49 +0530

    Update and rename even or odd program.c to even or odd program(if else).c

--- even or odd program(if else).c
+++ even or odd program(if else).c
@@ -3,13 +3,13 @@ int main(){
 int number=0;    
 printf("enter a number:");    
 scanf("%d",&number);     
-if(number%2==0)
+if(number%2==0)  // number divisible by 2 is even
 {    
 printf("%d is even number",number);    
 }    
 else
 {    
-printf("%d is odd number",number);    
+printf("%d is odd number",number);  // numbers other than even are taken as odd  
 }     
 return 0;  
 }    
