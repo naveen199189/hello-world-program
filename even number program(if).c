48854d80c89e7a82cecfefac9e666569d15817e8
naveen199189 <106946229+naveen199189@users.noreply.github.com>
2022-06-07 15:57:25 +0530

    Update and rename if.c to even number program(if).c

--- even number program(if).c
+++ even number program(if).c
@@ -4,7 +4,7 @@ int main()
 int number=0;    
 printf("Enter a number:");    
 scanf("%d",&number);    
-if(number%2==0)
+if(number%2==0) //number divisible by 2
 {    
 printf("%d is even number",number);    
 }    
