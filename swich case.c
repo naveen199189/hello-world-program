e5d0e7e1a6a0a84ba7c8925a6f363995abf35ba7
naveen199189 <106946229+naveen199189@users.noreply.github.com>
2022-06-08 10:51:27 +0530

    Create swich case.c

---
+++ swich case.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main()
+{
+int number=0;
+printf("enter a number:");
+scanf("%d",&number);
+switch(number)
+{
+case 10:
+printf("number is equal to 10");
+break;
+case 20:
+printf("number is equal to 20");
+break;
+case 30:
+printf("number is equal to 30");
+break;
+default:
+printf("number is not equal to 10 20 30 ");
+
+}
+    
+    return 0;
+}
