f206aacec75f909a1b683a56007530e0f9e57dfc
naveen199189 <106946229+naveen199189@users.noreply.github.com>
2022-06-20 12:21:57 +0530

    Add files via upload

---
+++ transpose.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+	int a[10][10],transpose[10][10],i,j,r,c;
+	printf("enter the size of row and column :\n");
+	scanf("%d%d",&r,&c);
+	
+	printf("enter the elements of matrix:\n");
+	for(i=0;i<r;i++)
+	for(j=0;j<c;j++)
+	{
+	printf("enter elements of a%d%d:",i+1,j+1);
+	scanf("%d",&a[i][j]);
+	}
+	
+	printf("matrix of the elements is :\n");
+	for(i=0;i<r;i++)
+	for(j=0;j<r;j++)
+	{
+	printf("%d",a[i][j]);
+	if(j==c-1)
+	printf("\n");
+	}
+
+	for(i=0;i<r;i++)
+	for(j=0;j<c;j++)
+	transpose [j][i]=a[i][j];
+
+	printf("\ntranspose of matrix :\n");
+	for(i=0;i<r;i++)
+	for(j=0;j<c;j++)
+	{
+        printf("%d",transpose[i][j]);
+	if(j==r-1)
+	printf("\n");
+	}
+	return 0;
+}
+
