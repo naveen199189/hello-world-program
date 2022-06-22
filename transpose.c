88000a31eb7f55692a463cfa5e7371f842a42975
naveen199189 <106946229+naveen199189@users.noreply.github.com>
2022-06-22 15:25:54 +0530

    Update transpose.c

--- transpose.c
+++ transpose.c
@@ -2,29 +2,29 @@
 int main()
 {
 	int a[10][10],transpose[10][10],i,j,r,c;
-	printf("enter the size of row and column :\n");
+	printf("enter the size of row and column :\n");   // declering the size of matrix
 	scanf("%d%d",&r,&c);
 	
-	printf("enter the elements of matrix:\n");
+	printf("enter the elements of matrix:\n");        // enter the the elements in the matrix
 	for(i=0;i<r;i++)
 	for(j=0;j<c;j++)
 	{
-	printf("enter elements of a%d%d:",i+1,j+1);
+	printf("enter elements of a%d%d:",i+1,j+1);       
 	scanf("%d",&a[i][j]);
 	}
 	
-	printf("matrix of the elements is :\n");
+	printf("matrix of the elements is :\n");   
 	for(i=0;i<r;i++)
 	for(j=0;j<r;j++)
 	{
 	printf("%d",a[i][j]);
-	if(j==c-1)
+	if(j==c-1)                          // condition print elements on next column
 	printf("\n");
 	}
 
 	for(i=0;i<r;i++)
 	for(j=0;j<c;j++)
-	transpose [j][i]=a[i][j];
+	transpose [j][i]=a[i][j];         // transpose condition
 
 	printf("\ntranspose of matrix :\n");
 	for(i=0;i<r;i++)
